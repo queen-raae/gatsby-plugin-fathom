8fac9dfde38cecebbfe24d0b8836d2c3ce3b3a89
Benedicte Raae <raae@bgraphic.no>
2021-01-17 20:33:35 +0100

    Initial commit

---
+++ gatsby-plugin-fathom/gatsby-node.js
@@ -0,0 +1,16 @@
+exports.pluginOptionsSchema = ({Joi}) => {
+  return Joi.object({
+    site: Joi.string().required().description(`The Fathom site id`),
+    honorDnt: Joi.boolean().description(`Honor Do Not Track?`),
+    auto: Joi.boolean().description(`Automatically track page views?`),
+    canonical: Joi.boolean().description(
+      `Use the canonical URL, instead of the current URL`
+    ),
+    excludedDomains: Joi.string().description(`Exclude these domains`),
+    includedDomains: Joi.string().description(`Include these domains`),
+    spa: Joi.string()
+      .valid("auto", "history", "hash")
+      .default("auto")
+      .description(`How to track page views`),
+  });
+};
