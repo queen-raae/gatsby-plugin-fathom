3de3ac9bb17356c8f0ef244f84b7236dc8b97944
Benedicte Raae <raae@raae.codes>
2021-02-15 11:20:14 +0100

    Readme ready

--- gatsby-plugin-fathom/gatsby-node.js
+++ gatsby-plugin-fathom/gatsby-node.js
@@ -1,17 +1,23 @@
-exports.pluginOptionsSchema = ({Joi}) => {
+exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
     site: Joi.string().required().description(`The Fathom site id`),
-    honorDnt: Joi.boolean().description(`Honor Do Not Track?`),
-    auto: Joi.boolean().description(`Automatically track page views?`),
-    canonical: Joi.boolean().description(
-      `Use the canonical URL, instead of the current URL`
+    honorDnt: Joi.boolean().default(false).description(`Honor Do Not Track?`),
+    auto: Joi.boolean()
+      .default(true)
+      .description(`Automatically track page views?`),
+    canonical: Joi.boolean()
+      .default(true)
+      .description(`Use the canonical URL, instead of the current URL?`),
+    excludedDomains: Joi.string().description(
+      `Exclude tracking for these domains`
     ),
-    excludedDomains: Joi.string().description(`Exclude these domains`),
-    includedDomains: Joi.string().description(`Include these domains`),
+    includedDomains: Joi.string().description(
+      `Include tracking for these domains`
+    ),
     spa: Joi.string()
       .valid("auto", "history", "hash")
       .default("auto")
-      .description(`How to track page views`),
+      .description(`Tracking mode`),
     loadType: Joi.string()
       .valid("defer", "async")
       .default("defer")
