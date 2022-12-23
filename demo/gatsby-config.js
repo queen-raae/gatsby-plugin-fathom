92d76df4b6b90ffc7065624a444a8e4d104ec4de
Benedicte Raae <raae@raae.codes>
2022-12-23 08:49:33 +0100

    chore: reorg + semantic release

---
+++ demo/gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: `@raae/gatsby-plugin-fathom`,
+      options: {
+        site: "CNOZPPSE",
+        loadType: "async",
+      },
+    },
+  ],
+}
